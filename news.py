ef619b7c1ba15fb9e54f1618333aeac263bd597f
Federico Palma <federico.palma@hotmail.com>
2020-04-24 04:02:49 -0300

    Add article Item

--- news.py
+++ news.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from items import Article
+
 
 class NewsSpider(scrapy.Spider):
     name = 'newsspider'
     start_urls = ['https://www.infobae.com']
 
     def parse(self, response):
-        for title in response.css('.headline'):
-            yield {'title': title.css('a::text').get()}
+        for el in response.css('.headline'):
+            url = response.url + el.css('a').attrib['href']
+
+            article = Article()
+            article['title'] = el.css('a::text').get().strip()
+            article['url'] = url
+            yield article
